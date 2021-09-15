b48ab0b4dc31a799129653324b90e73e096ca10c
t0kar <81991067+t0kar@users.noreply.github.com>
2021-09-15 19:22:39 +0200

    Add day.js and nanoid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as dayjs from "dayjs";
+import { nanoid } from "nanoid";
+
+require("dayjs/locale/hr");
+
+// MMMM DD YYYY, HH:mm:ss
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Učimo ReactJS</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Moja prva aplikacija</p>
+        <p>Danas je {dayjs().locale("hr").format("MMMM DD YYYY, HH:mm:ss")}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://en.wikipedia.org/wiki/React_(JavaScript_library)"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wikipedia o ReactJS-u
         </a>
+        <p>
+          Tvoj jedinstveni identifikator je <strong>{nanoid()}</strong>.
+        </p>
       </header>
     </div>
   );
 }
-
 export default App;
